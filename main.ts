a7efdb711502eab246d8177edf74efbade825889
FelixLuciano <felix.lucianodias@gmail.com>
2022-10-25 09:25:38 -0300

    feat: :sparkles: favicon encoding

--- main.ts
+++ main.ts
@@ -1,15 +1,18 @@
+import { encode } from "https://deno.land/std@0.160.0/encoding/base64.ts";
 import { transform } from "https://deno.land/x/esbuild@v0.15.11/mod.js";
 
 const source = await Deno.readTextFile("src/PlaybackRange.js");
 const template = await Deno.readTextFile("src/template.html");
 
-const { code } = await transform(source, {
+let { code } = await transform(source, {
   minify: true,
   platform: "browser",
   
 });
 
-const data = template.replace("${code}", code.replaceAll("\n", "").replaceAll(/\s+/g, " ").replaceAll("\"", "'"));
+let icon = encode(await Deno.readFile("assets/image/favicon.png"));
+
+const data = template.replace("${code}", encodeURI(code)).replace("${icon}", icon);
 
 try {
   await Deno.mkdir("dist");
